d264a0c92015d0b5592507fab3848e3eef329f0d
Cristian Lupu <cristian.lupu@ellation.com>
2018-09-22 19:53:31 +0300

    Support Travis CI (#10)
    Reviewer: cristian.lupu

--- Youth/Supporting Files/Bridging Header/Youth-Bridging-Header.h
+++ Youth/Supporting Files/Bridging Header/Youth-Bridging-Header.h
@@ -6,10 +6,5 @@
 //  Copyright © 2018 Lupu Cristian. All rights reserved.
 //
 
-// Maybe this bridging header will be useful 😀
-
-// Ohh...eeee...it became useful (maybe, i think so) ))
 #import "Youth_ObjCExceptionCatcherHelper.h"
-
 #import <DownloadButton/PKDownloadButton.h>
-
