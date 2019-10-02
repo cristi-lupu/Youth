c399f5f7f1a89659c5419fd2eae3b49f2fe4001f
cristi-lupu <cristian.lupu@ellation.com>
2019-10-02 20:36:14 +0300

    Update dependencies

--- Youth/Classes/Core Layer/Network/Unsplash/UnsplashRxExtensions.swift
+++ Youth/Classes/Core Layer/Network/Unsplash/UnsplashRxExtensions.swift
@@ -10,7 +10,7 @@ import RxSwift
 
 extension ObservableType {
     @discardableResult
-    func unsplashOnFailure(_ block: @escaping (Unsplash.Error) -> Void) -> Observable<E> {
+    func unsplashOnFailure(_ block: @escaping (Unsplash.Error) -> Void) -> Observable<Element> {
         return self.do(onError: { error in
             if let error = error as? Unsplash.Error {
                 block(error)
@@ -23,8 +23,8 @@ extension ObservableType {
     }
 }
 
-extension ObservableType where E: UnsplashResponseType {
-    func map<U>(_ transform: @escaping (E.Model) -> U) -> Observable<Unsplash.Response<U>> {
+extension ObservableType where Element: UnsplashResponseType {
+    func map<U>(_ transform: @escaping (Element.Model) -> U) -> Observable<Unsplash.Response<U>> {
         return map { $0.map(transform) }
     }
 }
